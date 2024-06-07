abc63d8004f0620097f1be3b262540fecc4dadd8
nguyenquoccuongcn18 <nguyenquoccuongcn18@gmail.com>
2024-06-07 23:15:02 +0700

    BaseObject_01

--- interfaces/pageUIs/HomePageUI.java
+++ interfaces/pageUIs/HomePageUI.java
@@ -1,4 +1,12 @@
 package pageUIs;
 
 public class HomePageUI {
+    //1. public : bất kì 1 class nào cũng có thể gọi đến để sử dụng
+    //2. static : không cần khởi tạo đối tượng để sử dụng
+    //3. final : không thể thay đổi giá trị
+    //4. String : kiểu dữ liệu chuỗi
+    //5. REGISTER_LINK : tên element_loại element
+    public static final String REGISTER_LINK = "//a[@class='ico-register']";
+
+
 }
