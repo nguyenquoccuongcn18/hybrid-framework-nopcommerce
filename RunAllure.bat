99a6c34e72f605c2882f20417b31eb5636dda0b7
nguyenquoccuongcn18 <nguyenquoccuongcn18@gmail.com>
2024-08-01 17:08:30 +0700

    Allure_report

--- RunAllure.bat
+++ RunAllure.bat
@@ -2,7 +2,6 @@ set ProjectPath=%~dp0
 cd %ProjectPath%
 echo %ProjectPath%
 set p=%PATH%
-java -javaagent:"%ProjectPath%\libAllure\aspectjweaver-1.9.20.jar" -classpath "%ProjectPath%bin;%ProjectPath%libAsserJ\*;;%ProjectPath%libAllure\*;%ProjectPath%libExtent5\*;%ProjectPath%libReportNG\*;%ProjectPath%libSelenium\*;" org.testng.TestNG "%ProjectPath%resources\RunReportAllure.xml"
-allure serve .\allure-results\
+java -javaagent:"%ProjectPath%libAllure\aspectjweaver-1.9.20.jar" -classpath "%ProjectPath%out\production\hybrid-framework-nopcommerce;%ProjectPath%libAllure\*;%ProjectPath%libLog4Jver2\*;%ProjectPath%libReportNG\*;%ProjectPath%libSelenium\*;%ProjectPath%libWebDriverManager\*" org.testng.TestNG "%ProjectPath%resources\RunReportAllure.xml"
+allure serve .\allure-json\
 pause
-
